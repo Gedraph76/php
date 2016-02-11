dfef531bd48af1b0f524159f2fb0f425eccd1c0f
Gedaph76 <gedraphkinyanjui3@gmail.com>
2016-02-11 21:59:46 +0300

    Introduction to PHP

---
+++ Lesson1/lesson1.php
@@ -0,0 +1,37 @@
+<?php
+//Introduction to php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executable on the server
+//PHP files can contain text, HTML, CSS, Javascript and PHP code
+//PHP code is executable on the server and the result is returned to the browser as plain HTML.
+//PHP files have extension ".php" a must
+echo "Hello World";
+echo "<h1>Hello World</h1>";
+//echo:print content in browser
+//hello world :the content to be printed on the browser
+//;means the end ot the statement you must finish with the semicolon
+
+
+//VARIABLES IN PHP
+//a container for storing date_add
+$myname ="John Doe <br>";
+echo $myname;
+$com1 ="Samsung <br>";
+$com2 ="Apple <br>";
+$com3 ="HassConsult <br>";
+$com4 ="Ctyonn <br>";
+echo $com1,$com2,$com3,$com4;
+
+
+
+
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ab consequuntur cumque earum eveniet, exercitationem illum
+    magni maiores, nesciunt obcaecati perspiciatis quas quod rem
+    temporibus, ullam voluptatum? Distinctio quod reiciendis sunt.
+</p>
